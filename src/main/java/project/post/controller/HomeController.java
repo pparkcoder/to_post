c52fbcb5e2a5d999e436a4d1e42ace1f93326bb2
parkjanghoon <pparkcoder@gmail.com>
2025-02-18 22:27:33 +0900

    @SessionAttribute 적용, 세션 타임아웃 설정

--- src/main/java/project/post/controller/HomeController.java
+++ src/main/java/project/post/controller/HomeController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import project.post.domain.Member;
 import project.post.repository.MemberRepository;
 import project.post.session.SessionConst;
@@ -27,14 +28,8 @@ public class HomeController {
 //    }
 
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model){
-        HttpSession session = request.getSession(false);
-
-        if(session == null){
-            return "main";
-        }
-
-        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+    public String homeLogin(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
 
         // 세션에 회원 데이터가 없으면 home
         if(loginMember == null){
